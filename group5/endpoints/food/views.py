329f07976b66c15ab2de1a432b6676ded76574af
KwonL <lkh116@snu.ac.kr>
2020-05-31 18:14:23 +0900

    hotfix
    hotfix
    
    hotfix

--- group5/endpoints/food/views.py
+++ group5/endpoints/food/views.py
@@ -16,10 +16,11 @@ class HomeScreenAPI(APIView):
         result = {"kor_name": user.kor_name}
         cnt = FoodGallery.objects.filter(user=user).count()
         # 3으로 나눴을 때 남는 음식을 가져옴
+        start = cnt - cnt % 3 if cnt % 3 else cnt - 4
         foods = (
             FoodGallery.objects.select_related("nutrient")
             .filter(user=user)
-            .order_by("id")[cnt - cnt % 3 :]
+            .order_by("id")[start:]
         )
         result.update(
             {"calories": [f.nutrient.kcal if f.nutrient else 0 for f in foods]}
